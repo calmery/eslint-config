d4e9134f978ed5d468bfff010921066434af8e97
Marei Kikukawa <12670155+calmery@users.noreply.github.com>
2021-03-23 00:25:54 +0900

    feat: ベースとなる ESLint の設定を追加する

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
-export default {};
+export = {
+  env: {
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:import/errors",
+    "plugin:import/typescript",
+    "plugin:import/warnings",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
+  root: true,
+  rules: {
+    "@typescript-eslint/explicit-module-boundary-types": "off",
+    "import/order": ["error", { alphabetize: { order: "asc" } }],
+  },
+  settings: {
+    "import/resolver": {
+      typescript: {},
+    },
+  },
+};
